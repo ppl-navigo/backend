b135ae04c2037ce139706d52e78420642952aadb
eryawww <zazaneryawan@gmail.com>
2025-03-03 21:07:33 +0700

    [REFACTOR] add legal_docs_generator router to main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.routers import health_check
+from app.routers.legal_docs_generator import deepseek, legal_docs
 
 app = FastAPI()
 app.include_router(health_check.router)
+app.include_router(deepseek.router)
+app.include_router(legal_docs.router)
 
 @app.get("/")
 async def root():
