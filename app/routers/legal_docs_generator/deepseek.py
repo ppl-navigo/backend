2527987bf35723e0e23b99941bba77e3482fb285
eryawww <zazaneryawan@gmail.com>
2025-03-06 23:40:10 +0700

    [REFACTOR] Temporary change endpoints to gemini API

--- app/routers/legal_docs_generator/deepseek.py
+++ app/routers/legal_docs_generator/deepseek.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 from app.routers.legal_docs_generator.dtos import DeepSeekRequest
+
 import ollama
+from google import genai
+from google.genai import types
+
+import os
 
 router = APIRouter()
 
@@ -9,18 +14,30 @@ async def deepseek_stream_response(system_prompt: str, query: str):
     """
     Streams responses from a locally deployed DeepSeek API through ollama.
     """
- 
-    stream = ollama.chat(
-        model='deepseek-r1:8b',
-        messages=[
-            {'role': 'system', 'content': system_prompt},
-            {'role': 'user', 'content': query}
-        ],
-        stream=True
+    # Gemini API Request
+    client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+    response = client.models.generate_content_stream(
+        model="gemini-2.0-flash-lite",
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt
+        ),
+        contents=[query]
     )
+    for chunk in response:
+        yield chunk.text
 
-    for chunk in stream:
-        yield chunk['message']['content']
+ 
+    # Deepseek Ollama Implementation
+    # stream = ollama.chat(
+    #     model='deepseek-r1:8b',
+    #     messages=[
+    #         {'role': 'system', 'content': system_prompt},
+    #         {'role': 'user', 'content': query}
+    #     ],
+    #     stream=True
+    # )
+    # for chunk in stream:
+    #     yield chunk['message']['content']
 
 @router.post("/deepseek", response_class=StreamingResponse)
 async def deepseek_generate(data: DeepSeekRequest):
