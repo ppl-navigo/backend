c10c4dc5105014e068aac624b3645c5be76d2f29
Bryan Tjandra <bryan.tjandra@ui.ac.id>
2025-03-13 12:15:11 +0700

    [CHORES] add logging

--- app/utils/ai_client.py
+++ app/utils/ai_client.py
@@ -14,7 +14,7 @@ class AIClient:
 
         # Combine extracted text with the stored risk analysis prompt
         full_prompt = f"{settings.AI_RISK_ANALYSIS_PROMPT}\n\n{text}"
-
+        print(full_prompt)
         try:
             response = client.chat.completions.create(
                 extra_headers={
@@ -32,7 +32,7 @@ class AIClient:
             )
 
             ai_output = response.choices[0].message.content.strip()
-
+            print(ai_output)
             # Temporary hardcode karena API sedang error, dan self deployed API in progress
             # ai_output = settings.AI_OUTPUT
 
