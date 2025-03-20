910550d137704078da8514fcf0ceb3a6099b71de
juankhusuma <juan.d.khusuma@gmail.com>
2025-03-21 01:11:41 +0700

    [GREEN] Passed all tests for dense retrieval

--- app/services/retrieval/DenseRetrieval.py
+++ app/services/retrieval/DenseRetrieval.py
@@ -1,6 +1,9 @@
 from app.services.retrieval.RetrievalStrategy import RetrievalStrategy
+from app.model.chatbot.legal_document import LegalDocumentChunk, LegalDocument
 from sqlmodel import Session
+from sqlalchemy.sql import func
 from app.config.settings import settings
+from sqlmodel import select
 from ollama import Client
 
 class DenseRetrieval(RetrievalStrategy):
@@ -9,7 +12,24 @@ class DenseRetrieval(RetrievalStrategy):
         super().__init__(db_session)
 
     def __embed_query(self, query: str) -> list[int]:
-        pass
+        return self.ollama_client.embed(model='bge-m3', input=query)[0]
 
     def retrieve(self, query: str):
-        pass
+        doc_embedding = self.__embed_query(query)
+        similarity_expr = 1 - (LegalDocumentChunk.embedding.op('<=>')(doc_embedding))
+        statement = (
+            select(
+                LegalDocumentChunk.page_number,
+                LegalDocumentChunk.legal_document_id,
+                func.max(similarity_expr).label("similarity")
+            )
+            .join(LegalDocument, LegalDocument.id == LegalDocumentChunk.legal_document_id)
+            .where(
+                LegalDocumentChunk.embedding.is_not(None),
+                similarity_expr > 0.5
+            )
+            .group_by(LegalDocumentChunk.page_number, LegalDocumentChunk.legal_document_id)
+            .order_by(func.max(similarity_expr).desc())
+            .limit(20)
+        )
+        return self.db_session.exec(statement).all()
