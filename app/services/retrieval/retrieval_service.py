0f7e002a9208b8ffb2d92d96bcb77ceb57fad235
juankhusuma <juan.d.khusuma@gmail.com>
2025-03-23 23:03:38 +0700

    [GREEN] Implemented retrieval services

--- app/services/retrieval/retrieval_service.py
+++ app/services/retrieval/retrieval_service.py
@@ -6,19 +6,28 @@ from typing import Annotated
 
 class RetrievalService:
     def __init__(self, strategy: RetrievalStrategy):
-        ...
+        self.strategy: RetrievalStrategy = strategy
 
     def retrieve(self, query: str):
-        ...
+        return self.strategy.retrieve(query)
     
 class RetrievalServiceFactory:
     def __init__(self, method: str):
-       ...
+        self.strategies = {
+            "dense": DenseRetrieval,
+            "sparse": SparseRetrieval,
+        }
+        if method is None:
+            raise ValueError("Method cannot be None")
+        if method not in self.strategies:
+            raise ValueError(f"Invalid retrieval method: {method}, must be one of {list(self.strategies.keys())}")
+        self.method = method
 
     def create(self, db: Session) -> RetrievalStrategy:
-        ...
+        strategy_class = self.strategies[self.method]
+        return strategy_class(db)
     
 def get_retrieval_strategy(method: str):    
-    ...
+    yield RetrievalService(RetrievalServiceFactory(method).create())
 
 RetrievalDep = Annotated[RetrievalService, get_retrieval_strategy]
