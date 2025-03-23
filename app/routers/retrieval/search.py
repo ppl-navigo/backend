474a276f1ed2d59fcaedcf238ca1cf559f9364cc
juankhusuma <juan.d.khusuma@gmail.com>
2025-03-24 03:21:54 +0700

    [GREEN] Finished implementing search endpoint

--- app/routers/retrieval/search.py
+++ app/routers/retrieval/search.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from app.commons.db.postgres import Postgres
+from app.services.retrieval.retrieval_service import RetrievalService, RetrievalServiceFactory
+from app.config.settings import settings
+
+postgres_db = Postgres(settings.DB_URL)
+def get_retrieval_strategy(method: str, db: Session = Depends(postgres_db.get_session)):    
+    yield RetrievalService(RetrievalServiceFactory(method).create(db))
 
 router = APIRouter()
 
 @router.get("/search")
-async def search():
-    ...
+async def search(query: str, retriever: RetrievalService = Depends(get_retrieval_strategy)):
+    return retriever.retrieve(query)
